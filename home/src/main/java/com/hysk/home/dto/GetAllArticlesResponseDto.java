1de8adeba81bd8086a68a139eb460a887dc90dcf
hysk-lhwr <joebeav@me.com>
2021-02-24 21:07:54 -0500

    add get all articles api

---
+++ home/src/main/java/com/hysk/home/dto/GetAllArticlesResponseDto.java
@@ -0,0 +1,12 @@
+package com.hysk.home.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GetAllArticlesResponseDto {
+    List<ShortenedArticle> articles;
+}
---
+++ home/src/main/java/com/hysk/home/service/ArticleService.java
@@ -0,0 +1,33 @@
+package com.hysk.home.service;
+
+import java.util.stream.Collectors;
+
+import com.hysk.home.dto.GetAllArticlesResponseDto;
+import com.hysk.home.dto.ShortenedArticle;
+import com.hysk.home.repository.ArticleRepository;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+    public final ArticleRepository articleRepository;
+
+    public ArticleService(final ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public ShortenedArticle getAllArticles() {
+        var articles = this.articleRepository.findAll();
+        var shortenedArticles = articles.stream().map(
+                entity -> ShortenedArticle.builder()
+                    .articleId(entity.articleId)
+                    .articleTitle(entity.title)
+                    .preview(entity.preview)
+                    .keywords(entity.keywords)
+                    .categories(entity.categories).build()
+            ).collect(Collectors.toList());
+
+        return GetAllArticlesResponseDto.builder().articles(shortenedArticles).build();
+    }
+    
+}
