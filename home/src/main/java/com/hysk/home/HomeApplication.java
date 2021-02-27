6e4960649754ab8c0cc7bba6b1f0dcacaf1eed7b
hysk-lhwr <joebeav@me.com>
2021-02-27 18:49:09 -0500

    add CorsOrigin configurer in the backend to allow frontend to connect from local

--- home/src/main/java/com/hysk/home/HomeApplication.java
+++ home/src/main/java/com/hysk/home/HomeApplication.java
@@ -1,12 +1,35 @@
 package com.hysk.home;
 
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HomeApplication {
 
+	@Value("${app.security.cors.allowedOrigins}")
+	private String[] allowedOrigins;
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+				.addMapping("/**")
+				.allowedOrigins(allowedOrigins)
+				.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(HomeApplication.class, args);
 	}
 
