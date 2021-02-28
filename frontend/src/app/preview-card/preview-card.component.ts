78650752d1ea33bdeaecb89d4b77344b7db35351
hysk-lhwr <joebeav@me.com>
2021-02-27 19:34:45 -0500

    a few style updates for preview card

--- frontend/src/app/preview-card/preview-card.component.ts
+++ frontend/src/app/preview-card/preview-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ShortenedArticle } from '../models/shortened-article';
 
 @Component({
   selector: 'app-preview-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview-card.component.scss']
 })
 export class PreviewCardComponent implements OnInit {
+  @Input('article') article: ShortenedArticle;
 
   constructor() { }
 
