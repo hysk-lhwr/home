9d474681e95781b4ae069c0278de671b5c7bb0bf
hysk-lhwr <joebeav@me.com>
2021-03-25 22:13:52 -0400

    add scroll to top button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { User } from './models/user';
 import { UserService } from './service/user.service';
 
@@ -7,16 +7,36 @@ import { UserService } from './service/user.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hysk-homepage';
   user: User;
+  @ViewChild('scrollToTopBtn', {static: false}) scrollToTopBtn: ElementRef;
 
   constructor(private userService: UserService) {
-
     this.userService.user$.subscribe(
       u => {
         this.user = u;
       }
     )
+  }
+
+  ngOnInit() {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const verticalOffset = window.pageYOffset 
+    || document.documentElement.scrollTop 
+    || document.body.scrollTop || 0;
+
+    if(verticalOffset > 600) {
+      this.scrollToTopBtn.nativeElement.classList.add('showBtn');
+    } else {
+      this.scrollToTopBtn.nativeElement.classList.remove('showBtn');
+    }
+  }
+
+  toTop(): void {
+    window.scroll(0,0);
   }
 }
