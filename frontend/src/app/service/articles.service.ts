d9db65d56c8e45b54ed23ad32b2388589da702d0
hysk-lhwr <joebeav@me.com>
2021-04-06 13:30:47 -0400

    hoster specific changes : do not push - jun

--- frontend/src/app/service/articles.service.ts
+++ frontend/src/app/service/articles.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { EnvironmentService } from "./environment.service";
 import { Observable } from "rxjs";
 import { ArticlesResponse } from "../models/articles-response";
@@ -13,11 +13,13 @@ export class ArticlesService {
     constructor(private http: HttpClient, private environmentService: EnvironmentService) {}
 
     getArticles(params: Params): Observable<ArticlesResponse> {
+        params = Object.assign({}, params, {username: 'hysk'});
         const fullUrl = this.environmentService.backendUrl + 'articles';
         return this.http.get<ArticlesResponse>(fullUrl, {params: params});
     }
 
     searchArticles(params: Params): Observable<ArticlesResponse> {
+        params = Object.assign({}, params, {username: 'hysk'});
         const fullUrl = this.environmentService.backendUrl + 'searchArticles';
         return this.http.get<ArticlesResponse>(fullUrl, {params: params});
     }
