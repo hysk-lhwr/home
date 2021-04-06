a2c0bac043a536b7dace753903761030c6e4c4bf
hysk-lhwr <joebeav@me.com>
2021-04-06 15:17:33 -0400

    hoster specific changes: do not merge - rukbat

--- frontend/src/app/service/articles.service.ts
+++ frontend/src/app/service/articles.service.ts
@@ -13,11 +13,13 @@ export class ArticlesService {
     constructor(private http: HttpClient, private environmentService: EnvironmentService) {}
 
     getArticles(params: Params): Observable<ArticlesResponse> {
+        params = Object.assign({}, params, {username: 'rukbat'});
         const fullUrl = this.environmentService.backendUrl + 'articles';
         return this.http.get<ArticlesResponse>(fullUrl, {params: params});
     }
 
     searchArticles(params: Params): Observable<ArticlesResponse> {
+        params = Object.assign({}, params, {username: 'rukbat'});
         const fullUrl = this.environmentService.backendUrl + 'searchArticles';
         return this.http.get<ArticlesResponse>(fullUrl, {params: params});
     }
